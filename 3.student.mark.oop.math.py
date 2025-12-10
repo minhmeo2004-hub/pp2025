d2c3dbb8f0b4c66b38c943131f15379564fda3ba
minh <minhmeopro2004@gmail.com>
2025-12-10 14:02:31 +0700

    update 4 practtice 3

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -1,27 +1,38 @@
+import math
+import numpy as np
+
 class Student:
     def __init__(self,id,name,dob):
         self.__id =id
         self.__name=name
         self.__dob=dob
+        self.__gpa=0.0
 
     def get_id(self):
         return self.__id
     def get_name(self):
         return self.__name
+    def set_gpa(self,gpa):
+        self.__gpa=gpa
+    def get_gpa(self):
+        return self.__gpa
     def __str__(self):
-        return f"StudentID:{self.__id} | Name:{self.__name} | DoB:{self.__dob}"
+        return f"StudentID:{self.__id} | Name:{self.__name} | DoB:{self.__dob} | GPA:{self.__gpa:.2f}"
     
 class Course:
-    def __init__(self,id,name):
+    def __init__(self,id,name,credits):
         self.__id=id
         self.__name=name
+        self.__credits=credits
 
     def get_id(self):
         return self.__id
     def get_name(self):
         return self.__name
+    def get_credits(self):
+        return self.__credits
     def __str__(self):
-        return f"courseID:{self.__id} | CourseName:{self.__name}"
+        return f"courseID:{self.__id} | CourseName:{self.__name} | Credits:{self.__credits}"
     
 class System:
     def __init__(self):
@@ -35,6 +46,7 @@ class System:
             while True:
                 course_id=int(input("Course ID: "))
                 course_name=input("Course name: ")
+                credits = int(input("Cresdits: "))
                 print("-------------------------")
 
                 duplicate = False
@@ -43,8 +55,13 @@ class System:
                         print("Can not duplicate ID or Name of course")
                         duplicate = True
                         break
+
+                if(credits<0):
+                    print("Credit can be lower than 0!!")
+                    duplicate=True
+
                 if not duplicate:
-                    course_infor= Course(course_id, course_name)
+                    course_infor= Course(course_id, course_name,credits)
                     self.courses.append(course_infor)
                     break
     
@@ -88,6 +105,37 @@ class System:
                     self.marks[s_id,select_course]=mark
                 break
     
+    def cal_GPA(self):
+        for student in self.students:
+            student_id=student.get_id()
+            marks_ls=[]
+            credits_ls=[]
+            for course in self.courses:
+                course_id=course.get_id()
+                key=(student_id,course_id)
+
+                if key in self.marks:
+                    mark=self.marks[key]
+                    credits=course.get_credits()
+                    marks_ls.append(mark)
+                    credits_ls.append(credits)
+
+            marks_array=np.array(marks_ls)
+            credits_array=np.array(credits_ls)
+
+            if len(credits_array)>0 and np.sum(credits_array)>0:
+                weight_sum=np.sum(marks_array*credits_array)
+                total_credits=np.sum(credits_array)
+                gpa=weight_sum/total_credits
+                student.set_gpa(gpa)
+            else:
+                student.set_gpa(0.0)
+
+    def sort_by_gpa(self):
+        self.cal_GPA()
+        self.students.sort(key=lambda s: s.get_gpa(), reverse=True)
+        print()
+
     def list_course(self):
         print("\n\n")
         print("_____Courses_____")
@@ -106,6 +154,7 @@ system= System()
 system.input_courses()
 system.input_student()
 system.input_marks()
+system.sort_by_gpa()
 system.list_course()
 system.list_student()
 system.list_display()
